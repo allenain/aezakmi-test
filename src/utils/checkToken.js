831d797aab5a6cbed12f181d7e5c2b5ed4349cce
popolino <polinadorina888@gmail.com>
2025-05-16 12:11:09 +0300

    deploy

--- src/utils/checkToken.js
+++ src/utils/checkToken.js
@@ -1,11 +1,13 @@
+const BASE_PATH = "/aezakmi-test";
+
 export function checkTokenOrRedirect() {
   const token = localStorage.getItem("accessToken");
   const expires = localStorage.getItem("tokenExpires");
   const isValid = token && Date.now() < Number(expires);
   const isAuthPage = window.location.pathname === "/auth";
 
-  if (!isValid && !isAuthPage) {
-    window.location.href = "/auth";
+  if (!isValid && !window.location.pathname.startsWith(`${BASE_PATH}/auth`)) {
+    window.location.href = `${BASE_PATH}/auth`;
   }
   if (isValid && isAuthPage) {
     window.location.href = "/currency";
--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,7 @@ module.exports = {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
     clean: true,
+    publicPath: "/aezakmi-test/",
   },
   module: {
     rules: [
